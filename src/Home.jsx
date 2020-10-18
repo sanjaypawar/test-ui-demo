7b356d8ec90c440ae173fe06c703e0a7f8629353
sanjay pawar <sanjayp@masterblocks.co.in>
2020-10-18 12:03:36 +0530

    added registration dialog form on appbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,51 +6,123 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
+import Typography from '@material-ui/core/Typography';
+import Search from '@material-ui/icons/Search';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Grid from '@material-ui/core/Grid';
+
 
 import LogoImage from './img/logo.png';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    appBar: {
-        backgroundColor: '#fafafa',
-    },
-    icon: {
-        width: 100,
-        height: 50,
-    },
-  }));
-
+const useStyles = makeStyles((theme) => {
+    return ({
+        TypographyStyles: {
+            flex: 1
+        },
+        button: {
+            backgroundColor: '#0000ff',
+            margin: theme.spacing(1),
+            borderRadius: '50px',
+            color: 'white',
+        },
+    });
+});
 const StyledIcon = withStyles({
-    root: {
-        width: '8em',
-        height: '1.2em',
-    },
-    title: {
-        flexGrow: 1,
-    },
+  root: {
+      width: '8em',
+      height: '1.2em',
+  },
 })(Icon);
 
-export default function Home(){
-    const classes = useStyles();
 
-    return(
-        <div className={classes.root}>
-            <AppBar position="fixed" className={classes.appBar}>
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <StyledIcon>
-                            <img src={LogoImage} alt="logo" width="100%" height="100%"/>
-                        </StyledIcon>
-                    </IconButton>
-                    <TextField className={classes.title}/>
-                    <Button >Register</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+const Home =() =>{
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+const handleClickOpen = () =>{
+    setOpen(true);
 };
+const handleClose = () =>{
+    setOpen(false);
+};
+  return (
+    <AppBar position="fixed" color="default">
+      <Toolbar>
+        <Typography className= {classes.TypographyStyles}>
+            <IconButton edge="start" aria-label="menu">
+                <StyledIcon>
+                    <img src={LogoImage} alt="logo" width="100%" height="100%"/>
+                </StyledIcon>
+            </IconButton>
+        </Typography>
+        <TextField/>
+        <Search/>
+        <Button>Sign In</Button>
+        <Button size="small" className= {classes.button} onClick={handleClickOpen}>+ Register</Button>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogContent>
+          <DialogContentText>
+            <h1>Register</h1>
+          </DialogContentText>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="First Name"
+                    type="text"
+                    fullWidth
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Last Name"
+                    type="text"
+                    fullWidth
+                    />
+                </Grid>
+            </Grid>
+            <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email"
+            type="email"
+            fullWidth
+            />
+            <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Password"
+            type="password"
+            fullWidth
+            />
+            <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Confirm Password"
+            type="password"
+            fullWidth
+            />
+        </DialogContent>
+        <DialogActions>
+          {/* <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button> */}
+          <Button fullWidth='false' variant="contained" color="primary">
+            Register Now
+          </Button>
+        </DialogActions>
+      </Dialog>
+      </Toolbar>
+    </AppBar>
+  );
+};
+export default Home;
